5b222f4053e4a8ba5d7b51a9953218d217874b2c
Bella Dear <arabelladear@hotmail.co.uk>
2018-07-10 11:49:18 +0100

    homepage in front end working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+
+import 'bulma';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>WDI32 Project 4</h1>
+      <BrowserRouter>
+        <main>
+          <Route exact path='/' component={Home} />
+          <section className="section">
+            <div className="container">
+              <Switch>
+
+              </Switch>
+            </div>
+          </section>
+        </main>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Find recommendations</h1>
+      <input className="input" placeholder="Enter a city..."/>
+    </main>
+  );
+};
+
+export default Home;
