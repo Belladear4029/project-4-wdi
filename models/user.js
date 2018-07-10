94f3b483ecec0180ff967df2a640d12eb5fa5dff
Bella Dear <arabelladear@hotmail.co.uk>
2018-07-10 10:10:23 +0100

    user model completed

--- models/user.js
+++ models/user.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  image: { type: String },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  following: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+  followees: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
 }, {
   id: false
 });
