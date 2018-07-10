222a656e3a9ccabbc7e566382df72d8dc5ee9d60
Bella Dear <arabelladear@hotmail.co.uk>
2018-07-10 14:43:15 +0100

    register page working

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Navbar from './components/common/Navbar';
+import AuthRegister from './components/auth/Register';
+// import AuthLogin from './components/auth/Login';
 import CitiesShow from './components/cities/Show';
 import UsersIndex from './components/users/Index';
 import UsersShow from './components/users/Show';
@@ -23,6 +25,8 @@ class App extends React.Component {
                 <Route path="/cities/:id" component={CitiesShow}/>
                 <Route path="/users/:id" component={UsersShow}/>
                 <Route path="/users" component={UsersIndex}/>
+                <Route path="/register" component={AuthRegister}/>
+                {/* <Route path="/login" component={AuthLogin}/> */}
               </Switch>
             </div>
           </section>
---
+++ src/components/auth/Register.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import axios from 'axios';
+
+class AuthRegister extends React.Component {
+
+  state = {}
+
+  handleChange = ({ target: { name, value }}) => {
+    this.setState({ [name]: value });
+    console.log(this.state);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      url: '/api/register',
+      method: 'POST',
+      data: this.state
+    })
+      .then(() => {
+        this.props.history.push('/login');
+      });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label className="firstName">First Name</label>
+          <input className="input" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+        </div>
+        <div className="field">
+          <label className="lastName">Last Name</label>
+          <input className="input" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+        </div>
+        <div className="field">
+          <label className="email">Email</label>
+          <input className="input" name="email" placeholder="Email" onChange={this.handleChange} />
+        </div>
+        <div className="field">
+          <label className="password">Password</label>
+          <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange} />
+        </div>
+        <div className="field">
+          <label className="passwordConfirmation">Password Confirmation</label>
+          <input className="input" type="password" name="passwordConfirmation" placeholder="Password Confirmation" onChange={this.handleChange} />
+        </div>
+
+        <button className="button">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default AuthRegister;
