18898f26f4128bb5820ea75837c74ed77887d88e
Bella Dear <arabelladear@hotmail.co.uk>
2018-07-10 13:39:38 +0100

    more work to user show page - added titles

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   following: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
-  followees: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
+  followers: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
 }, {
   id: false
 });
--- src/components/users/Show.js
+++ src/components/users/Show.js
@@ -13,14 +13,23 @@ class UsersShow extends React.Component {
   componentDidMount() {
     axios.get(`/api/users/${this.props.match.params.id}`)
       .then(res => this.setState({ user: res.data }))
-      .then(() => console.log(this.state.user.firstName));
+      .then(() => console.log(this.state.user));
   }
 
   render() {
     return (
-      <div>
-        <img src={this.state.user.image} />
-        <h1>{this.state.user.firstName}</h1>
+      <div className="columns is-multiline">
+        <div className="column is-half-desktop">
+          <img src={this.state.user.image} />
+          <h1 className="title is-2">{this.state.user.firstName} {this.state.user.lastName}</h1>
+        </div>
+        <div className="column is-half-desktop">
+          <p className="title is-5">{this.state.user.followers} followers</p>
+          <p className="title is-5">{this.state.user.following} following</p>
+        </div>
+        <div className="column is-full-desktop">
+          <p className="title is-5">Recommendations</p>
+        </div>
       </div>
     );
   }
