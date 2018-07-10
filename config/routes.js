8f5fbc27b265cdb18fcde756b13b0a8fd84e161f
Bella Dear <arabelladear@hotmail.co.uk>
2018-07-10 11:27:56 +0100

    user show route in backend working

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,7 @@ const users = require('../controllers/users');
 router.get('/cities/:id', cities.show);
 
 router.get('/users', users.index);
+router.get('/users/:id', users.show);
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,19 @@ const User = require('../models/user');
 function indexRoute(req, res, next) {
   User
     .find()
+    .then(users => res.json(users))
+    .catch(next);
+}
+
+function showRoute(req, res, next) {
+  User
+    .findById(req.params.id)
     .then(user => res.json(user))
     .catch(next);
 }
 
+
 module.exports = {
-  index: indexRoute
+  index: indexRoute,
+  show: showRoute
 };
