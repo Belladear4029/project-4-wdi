4044959e7ab5657ff415a646d5fa61930ea466be
Bella Dear <arabelladear@hotmail.co.uk>
2018-09-04 14:40:33 +0100

    changed oanda key

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,8 @@ const port = process.env.PORT || 4000;
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/iRecommend';
 const secret = '+xens#,]{)h34aV&';
 const yandexKey = process.env.YANDEX_API_KEY;
-const oandaKey = process.env.OANDA_API_KEY;
+const oandaKey = 'TkmrkLJaUG8cCtWg2CIxXYta';
+console.log(yandexKey);
+console.log(oandaKey);
 
 module.exports = { port, dbURI, secret, yandexKey, oandaKey };
